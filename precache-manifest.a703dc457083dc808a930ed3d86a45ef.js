cee61929af0ae9d95acbd8a4f5609cb00609406b
daniel tegenrot <daniel@tegenrot.se>
2020-09-11 19:12:11 +0200

    Updates

---
+++ precache-manifest.a703dc457083dc808a930ed3d86a45ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73092ae998d4b1f62342b1e51adf5fbe",
+    "url": "/find-your-monster/index.html"
+  },
+  {
+    "revision": "2f608f173d53a557fdff",
+    "url": "/find-your-monster/static/css/main.aa863363.chunk.css"
+  },
+  {
+    "revision": "c88c289c522e0ee04b2d",
+    "url": "/find-your-monster/static/js/2.5f8eb70f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-your-monster/static/js/2.5f8eb70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f608f173d53a557fdff",
+    "url": "/find-your-monster/static/js/main.2a149841.chunk.js"
+  },
+  {
+    "revision": "d9dbcf032690056627a4",
+    "url": "/find-your-monster/static/js/runtime-main.16780d35.js"
+  }
+]);
